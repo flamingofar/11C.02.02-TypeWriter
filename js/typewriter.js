0d66401149186e0e423b8a2025f9db22e1edb51e
malte <malt0533@stud.kea.dk>
2022-02-08 10:50:13 +0100

    Basic effekt working

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,1 +1,24 @@
+/** @format */
+
 "use strict";
+const typeWriter = document.querySelector(".typewritten");
+let count = 0;
+let text = typeWriter.innerHTML.trim();
+window.addEventListener("DOMContentLoaded", setup);
+
+function setup() {
+	typeWriter.innerHTML = "";
+	init();
+}
+
+function init() {
+	if (count < text.length) {
+		setTimeout(loopFunc, 500);
+	}
+}
+
+function loopFunc() {
+	typeWriter.innerHTML += `${text.charAt(count)}`;
+	count++;
+	init();
+}
